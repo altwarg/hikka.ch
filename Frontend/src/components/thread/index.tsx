220ca196f5e7de835a5f5829911a585a5bfbf640
M. Becker <altwarg@outlook.com>
2020-04-05 18:22:26 +0300

    Scrolling to post on page load

--- Frontend/src/components/thread/index.tsx
+++ Frontend/src/components/thread/index.tsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 import { Link } from 'react-router-dom';
 import { Card } from 'react-bootstrap';
 
@@ -11,23 +11,31 @@ type Props = Readonly<{
     inThread: boolean;
 }>;
 
-export const Thread: React.FC<Props> = ({ info, className, inThread }) => (
-    <Card className={className}>
-        <Card.Header>
-            <span className="font-weight-bold mr-3">{info.Title}</span>
-            <span>Posts: {info.PostsCount}</span>
-            { !inThread && <Link className="float-right font-weight-bold" to={`/${info.Board}/${info.Id}`}>Open thread</Link>}
-        </Card.Header>
-        <Card.Body>
-            {info.Posts.map((item, key) => (
-                <Post
-                    info={item}
-                    board={info.Board}
-                    id={info.Id}
-                    key={key}
-                    className={key !== info.Posts.length - 1 ? 'mb-3' : ''}
-                />
-            ))}
-        </Card.Body>
-    </Card>
-);
+export const Thread: React.FC<Props> = ({ info, className, inThread }) => {
+    useEffect(() => {
+        if (inThread) {
+            document.querySelector(document.location.hash)!.scrollIntoView();
+        }
+    }, [inThread]);
+
+    return (
+        <Card className={className}>
+            <Card.Header>
+                <span className="font-weight-bold mr-3">{info.Title}</span>
+                <span>Posts: {info.PostsCount}</span>
+                {!inThread && <Link className="float-right font-weight-bold" to={`/${info.Board}/${info.Id}`}>Open thread</Link>}
+            </Card.Header>
+            <Card.Body>
+                {info.Posts.map((item, key) => (
+                    <Post
+                        info={item}
+                        board={info.Board}
+                        id={info.Id}
+                        key={key}
+                        className={key !== info.Posts.length - 1 ? 'mb-3' : ''}
+                    />
+                ))}
+            </Card.Body>
+        </Card>
+    )
+};
